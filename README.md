e5540a7061b47dee5f0f6cb16e45073d5ffa73a6
Pegos <steliospegos08@gmail.com>
2022-06-01 03:10:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# FiveM
+
+An advanved API for getting FiveM server info.
+Rqeuires axios
+
+## Documentation & Examples
+**Examples**
+Coming Soon
+
+## **ALL FUNCTION REQUESTS**
+- getServerStatus - Checks if the server is online or not - returns boolean
+- getPlayerAmount - Gets the active players within the server - (number)
---
+++ index.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+
+const DEFAULTS = {
+	timeout: 15000,
+    ip: 0000,
+    port: 30120,
+};
+
+class fivemServer {
+    constructor(ip, options) {
+        
+        if (!ip) {
+            throw new Error('No IP address provided');
+        }
+        
+        this.ip = ip || DEFAULTS.ip;
+        this.port = options.port || DEFAULTS.port;
+        this.options = Object.assign({}, DEFAULTS, options);
+    }
+
+    getServerStatus() {
+        return new Promise((send, err) => {
+            axios
+                .get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+                .then(function(body) {
+                    let server_status = {
+                        online: true,
+                    }
+                    send(server_status);
+                })
+                .catch(function(error) {
+                    let server_status = {
+                        online: false,
+                        url: error.config.url,
+                        method: error.config.method
+                    }
+                    if (error.response == undefined) send(server_status)
+                });
+        });
+    }
+
+    getPlayerAmount() {
+        return new Promise((send, err) => {
+            axios
+                .get(`http://${this.ip}:${this.port}/players.json`, { timeout: this.options.timeout })
+                .then(function(body) {
+                    let players = body.data;
+                    send(players.length);
+                })
+                .catch(function(error) {
+                    err(error);
+                });
+        });
+    }
+    
+    // TODO: Make this work
+    // getVersion() {
+	// 	return new Promise((send, err) => {
+	// 		axios
+	// 			.get(`http://${this.ip}:${this.port}/info.json`, { timeout: this.options.timeout })
+	// 			.then(function(body) {
+	// 				let version = body.data;
+	// 				send(version);
+	// 			})
+	// 			.catch(function(error) {
+	// 				err(error);
+	// 			});
+	// 	});
+	// }	
+}
+
+module.exports.fivemServer = fivemServer;
